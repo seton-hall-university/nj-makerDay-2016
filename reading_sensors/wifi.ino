a66a96669084effb7da82fa572ca0c6779aae1ce
G. Lee Clark, II <gclark2@gmail.com>
2016-03-05 20:37:07 -0500

    Removes sendWiFiAddress()

--- reading_sensors/wifi.ino
+++ reading_sensors/wifi.ino
@@ -41,63 +41,63 @@ char getWiFiAddress(void) {
   return WiFi.localIP();
 }
 
-void sendWiFiAddress(void) {
-  IPAddress ip = WiFi.localIP();
-  byte oct1 = ip[0];
-  byte oct2 = ip[1];
-  byte oct3 = ip[2];
-  byte oct4 = ip[3];
-  char s[16];
-  sprintf(s, "%d.%d.%d.%d", oct1, oct2, oct3, oct4);
-  Serial.print("IP \"char\" = ");
-  Serial.println(s);
-  
-  const char* serverHost = "maker.ifttt.com";
-  const int serverPort = 443;
-  String serverPath = "/trigger/esp8266-01-connected/with/key/bIZaJeZz4iLcCUnI9wkCQ6";
-  String serverPayload = String("POST ") + serverPath + " HTTP/1.1\r\n" +
-  "Host: " + serverHost + "\r\n" + "User-Agent: BuildFailureDetectorESP8266\r\n" +
-  "Connection: close\r\n\r\n" + "{\"value1\":\"" + s + "\"}";
-
-  Serial.println("\nSending IP Address...");
-  
-  
-//  Serial.print("Client Connected? ");
-//  Serial.println(client.connect(serverHost, serverPort));
-  
-  if (sslclient.connect(serverHost, serverPort)) {
-    Serial.print("Client Connected? ");
-    Serial.println(sslclient.connected());
-    
-    Serial.print("connecting to ");
-    Serial.println(serverHost);
-    Serial.print("requesting URL: ");
-    Serial.println(serverPath);
-    Serial.print("posting: ");
-    Serial.println(serverPayload);
-    
-    sslclient.print(serverPayload);
-    
-    Serial.println("request sent");
-    while (sslclient.connected()) {
-      String line = sslclient.readStringUntil('\n');
-      if (line == "\r") {
-        Serial.println("headers received");
-        break;
-      }
-    }
-    
-    String line = sslclient.readStringUntil('\n');
-    
-    Serial.println("Reply was:");
-    Serial.println("==========");
-    Serial.println(line);
-    Serial.println("==========");
-    Serial.println("Closing connection");
-    
-  } else {
-    // You didn't get a connection to the server:
-    Serial.println("\nConnection failed");
-  }
-}
+//void sendWiFiAddress(void) {
+//  IPAddress ip = WiFi.localIP();
+//  byte oct1 = ip[0];
+//  byte oct2 = ip[1];
+//  byte oct3 = ip[2];
+//  byte oct4 = ip[3];
+//  char s[16];
+//  sprintf(s, "%d.%d.%d.%d", oct1, oct2, oct3, oct4);
+//  Serial.print("IP \"char\" = ");
+//  Serial.println(s);
+//  
+//  const char* serverHost = "maker.ifttt.com";
+//  const int serverPort = 443;
+//  String serverPath = "/trigger/esp8266-01-connected/with/key/bIZaJeZz4iLcCUnI9wkCQ6";
+//  String serverPayload = String("POST ") + serverPath + " HTTP/1.1\r\n" +
+//  "Host: " + serverHost + "\r\n" + "User-Agent: BuildFailureDetectorESP8266\r\n" +
+//  "Connection: close\r\n\r\n" + "{\"value1\":\"" + s + "\"}";
+//
+//  Serial.println("\nSending IP Address...");
+//  
+//  
+////  Serial.print("Client Connected? ");
+////  Serial.println(client.connect(serverHost, serverPort));
+//  
+//  if (sslclient.connect(serverHost, serverPort)) {
+//    Serial.print("Client Connected? ");
+//    Serial.println(sslclient.connected());
+//    
+//    Serial.print("connecting to ");
+//    Serial.println(serverHost);
+//    Serial.print("requesting URL: ");
+//    Serial.println(serverPath);
+//    Serial.print("posting: ");
+//    Serial.println(serverPayload);
+//    
+//    sslclient.print(serverPayload);
+//    
+//    Serial.println("request sent");
+//    while (sslclient.connected()) {
+//      String line = sslclient.readStringUntil('\n');
+//      if (line == "\r") {
+//        Serial.println("headers received");
+//        break;
+//      }
+//    }
+//    
+//    String line = sslclient.readStringUntil('\n');
+//    
+//    Serial.println("Reply was:");
+//    Serial.println("==========");
+//    Serial.println(line);
+//    Serial.println("==========");
+//    Serial.println("Closing connection");
+//    
+//  } else {
+//    // You didn't get a connection to the server:
+//    Serial.println("\nConnection failed");
+//  }
+//}
 
