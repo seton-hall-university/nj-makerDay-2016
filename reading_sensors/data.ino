9a909122bf9ecbc797f21fb2eccefe4f96ab5976
G. Lee Clark, II <gclark2@gmail.com>
2016-04-13 22:07:33 -0400

    Adds data helper functions

--- reading_sensors/data.ino
+++ reading_sensors/data.ino
@@ -1,4 +1,5 @@
 // https://github.com/esp8266/Arduino/blob/master/doc/filesystem.md
+// Reference: Examples/ESP8266WebServer/FSBrowser
 
 // Libraries
 #include "Arduino.h"
@@ -11,16 +12,57 @@ bool checkMount() {
   if (!SPIFFS.begin()) {
     Serial.println("Failed to mount file system");
     return false;
-//  } else {
-//    Serial.println("We got files!");
-//    return;
+  } else {
+    Serial.println("We got files!");
+    return true;
   }
+}
+
+//format bytes
+String formatBytes(size_t bytes){
+  if (bytes < 1024){
+    return String(bytes)+"B";
+  } else if(bytes < (1024 * 1024)){
+    return String(bytes/1024.0)+"KB";
+  } else if(bytes < (1024 * 1024 * 1024)){
+    return String(bytes/1024.0/1024.0)+"MB";
+  } else {
+    return String(bytes/1024.0/1024.0/1024.0)+"GB";
+  }
+}
+
+String getContentType(String filename){
+  if(server.hasArg("download")) return "application/octet-stream";
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+//get heap status, analog input value and all GPIO statuses in one json call
+void loadStatus() {
+  String json = "{";
+  json += "\"heap\":"+String(ESP.getFreeHeap());
+  json += ", \"analog\":"+String(analogRead(A0));
+  json += ", \"gpio\":"+String((uint32_t)(((GPI | GPO) & 0xFFFF) | ((GP16I & 0x01) << 16)));
+  json += "}";
+  server.send ( 200, "application/json", json );
+  json = String();
 }
 
 // Load the index HTML file
 bool loadIndex() {
   // checkMount();
-  File indexFile = SPIFFS.open("/data/index.html", "r");
+  File indexFile = SPIFFS.open("/index.html", "r");
   if (!indexFile) {
     Serial.println("Failed to open index file");
     return false;
